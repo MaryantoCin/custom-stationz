55ea8928f7cfceafaa76feb87c0fa98911c345d1
Maryanto Cin <firevolt719@gmail.com>
2022-01-11 15:20:43 +0700

    merge login, register, reset frontend

--- resources/views/temp.blade.php
+++ resources/views/temp.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href=".{{ asset('css/style.css') }}">
     <title>Sign Up</title>
 </head>
 
@@ -19,11 +19,10 @@
                 alt="">
         </div>
         <div class="col-md-5 m-5">
-            <div class="d-block text-end"><small>Not a member? <a href="{{ route('register') }}">Sign up
-                        now</a></small>
-            </div>
-            <div class="d-flex px-5">
-                <form action="{{ route('login') }}" method="get">
+            <div class="d-block text-end"><small>Not a member? <a href="">Sign up now</a></small></div>
+            <form method="POST" action="{{ route('password.email') }}" class="px-5">
+                @csrf
+                <div class="d-flex">
                     <button class="display-none me-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-chevron-left" viewBox="0 0 16 16">
@@ -31,16 +30,13 @@
                                 d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
                         </svg>
                     </button>
-                </form>
-                <h4 class="text-center mt-5 mb-5"><b>Forget Password</b></h4>
-            </div>
-            <!-- <button class="mt-3 mb-3 btn btn-primary mx-auto">Sign up with Google</button> -->
-            <!-- <div class="position-relative">
+                    <h4 class="text-center mt-5 mb-5"><b>Forget Password</b></h4>
+                </div>
+                <!-- <button class="mt-3 mb-3 btn btn-primary mx-auto">Sign up with Google</button> -->
+                <!-- <div class="position-relative">
                     <hr>
                     <div class="position-absolute floating-text">OR</div>
                 </div> -->
-            <form method="POST" action="{{ route('password.email') }}" class="px-5">
-                @csrf
                 <div class="mb-5">
                     Enter the email address you used when you joined and we’ll send you instructions to reset your
                     password. For security reasons, we do NOT store your password. So rest assured that we will never
@@ -68,4 +64,4 @@
     </section>
 </body>
 
-</html>
+</html>l
